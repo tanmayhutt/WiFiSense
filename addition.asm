32deff5017b05132fdac4c1695837e7ca86e6a51
Tanmay Tiwari <89214022+tanmayhutt@users.noreply.github.com>
2026-06-09 03:39:56 +0530

    Update addition.asm

--- addition.asm
+++ addition.asm
@@ -1,9 +1,9 @@
-ORG 0000H         ; Reset vector address
-SJMP 30H          ; Short jump to the main program area to skip vector table
+ORG 0000H
+SJMP 0030H        ; Jump to main program
 
-ORG 30H           ; Main program starts at address 0030H
-MOV R1, #03H      ; Load immediate value 3 into Register 1
-MOV A, #02H       ; Load immediate value 2 into the Accumulator
-ADD A, R1         ; Add R1 to A (A = A + R1) -> A = 2 + 3
+ORG 0030H
+MOV R1, #03H      ; Load 3 into Register 1
+MOV A, #02H       ; Load 2 into Accumulator
+ADD A, R1         ; A = A + R1 (Result: A = 05H)
 
-END               ; End of assembly code
+END
